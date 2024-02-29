54122314fc58f8dcbdc05506639dcfdafeb35c62
Efrem Bonfiglio <efrem.bonfiglio@beefree.io>
2024-02-29 08:33:50 +0100

    fix

--- src/assets/js/constants.js
+++ src/assets/js/constants.js
@@ -48,7 +48,7 @@ export const galleryNextBtn = document.querySelector('.gallery-header__next-btn'
 export const SPACE_ID = 'sheep-game-space'
 export const SHEEP_GAME_BODY_CLASS = 'sheep-game'
 export const WAIT_BEFORE_GAME = 8 // seconds to wait before the game starts
-export const MAX_SHEEP = 50 // Max number of sheep painted on screen
+export const MAX_SHEEP = 10 // Max number of sheep painted on screen
 export const SHEEP_DRAW_INTERVAL = 5 // seconds
 
 // Invitation banner
